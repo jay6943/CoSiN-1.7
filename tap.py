e21559c72fd6fd345f41d45fc0b78877127a2324
jay6943 <uetri@naver.com>
2023-01-05 14:00:26 +0900

    .

--- tap.py
+++ tap.py
@@ -20,32 +20,52 @@ def units(x, y, wg, xsign):
   x5, y5 = dxf.move(idev, x2, y2, x4, y4, 0, 0, -dci.tilt * xsign)
 
   return x5, y5
-  
-def device(x, y):
 
-  wg = 0.42
+def lines(x, y, length, wg):
 
-  x1, _ = dev.sline(x, y, 100)
+  x1, _ = dev.sline(x, y, length)
   x2, _ = dev.taper(x1, y, 100, cfg.wg, wg)
   x3, _ = dev.srect(x2, y, 100, wg)
   x4, _ = dev.srect(x3, y, 100, wg)
   x5, _ = dev.taper(x4, y, 100, wg, cfg.wg)
-  x6, _ = dev.sline(x5, y, 100)
+  x6, _ = dev.sline(x5, y, length)
 
-  x7, y7 = units(x3, y, wg, -1)
-  x7, y7 = units(x3, y, wg,  1)
+  return x6, y, x3
 
-  return x6, x7, y7
+def device(x, y):
 
-def chip(x, y, lchip):
+  wg = 0.42
+
+  x1, _, x2 = lines(x, y, 100, wg)
+
+  x3, y3 = units(x2, y, wg, -1)
+  x3, y3 = units(x2, y, wg,  1)
+
+  return x1, x3, y3
+
+def chip(x, y, dc, lchip):
   
-  wg = 0.39
+  # dc = 2.6
+
+  wg = 0.42
   ch = 50
+  dy = ch * 2 + cfg.dc - dc
   y1 = y + ch
   y2 = y - ch
+  y3 = y1 + cfg.dc - dc
 
   idev = len(cfg.data)
-  x1, _ = dci.device(x, y, wg, ch)
+
+  x1, _, x2 = lines(x, y1, 300, wg)
+
+  x3, _ = dci.units(x2, y3, wg, dy, -1, -1)
+  x3, _ = dci.units(x2, y3, wg, dy,  1, -1)
+
+  l = (x2 - x) - (x3 - x2)
+
+  dev.sline(x,  y2, l)
+  dev.sline(x3, y2, l)
+
   x3, x4, ltip = dev.center(idev, x, x1, lchip)
 
   x5, t1 = tip.fiber(x3, y1, ltip, -1)
@@ -53,7 +73,7 @@ def chip(x, y, lchip):
   x6, t2 = tip.fiber(x4, y1, ltip, 1)
   x6, t2 = tip.fiber(x4, y2, ltip, 1)
 
-  s = 'tap-' + str(round(cfg.dc, 2))
+  s = 'tap-' + str(round(dc, 2))
   dev.texts(t1, y, s, 0.2, 'lc')
   dev.texts(t2, y, s, 0.2, 'rc')
   print(s, round(x4 - x3), round(x6 - x5))
@@ -62,11 +82,7 @@ def chip(x, y, lchip):
 
 def chips(x, y, arange):
 
-  var = cfg.dc
-
-  for cfg.dc in arange: _, y = chip(x, y, xsize)
-
-  cfg.dc = var
+  for dc in arange: _, y = chip(x, y, dc, xsize)
 
   return x + xsize, y
 
@@ -74,6 +90,6 @@ if __name__ == '__main__':
 
   # chip(0, 0, 3000)
 
-  chips(0, 0, dev.arange(1.16, 1.46, 0.1))
+  chips(0, 0, dev.arange(2.4, 2.7, 0.1))
 
   dev.saveas(cfg.work + 'tap')
